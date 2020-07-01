ce26e3faa0f234d5c677f3cd520596aac124a0b7
Hrishabh Kumar <hrishabh0007@gmail.com>
2020-07-01 22:51:44 +0530

    Updates

---
+++ precache-manifest.e4457c58e0e81596d91ee991c118b4f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b5b65bddd5cbbf84ae79379c627eb8",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "d2f31704b73fc1319488",
+    "url": "/First-React-App/static/css/main.db20559e.chunk.css"
+  },
+  {
+    "revision": "3e7b6e968cbbb62b8e77",
+    "url": "/First-React-App/static/js/2.b1a14f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-App/static/js/2.b1a14f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f31704b73fc1319488",
+    "url": "/First-React-App/static/js/main.a7779075.chunk.js"
+  },
+  {
+    "revision": "cd6c318414dc9bf1b0e3",
+    "url": "/First-React-App/static/js/runtime-main.a78ab8df.js"
+  }
+]);
